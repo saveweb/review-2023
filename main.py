fcf0f45a3ddb993060814d14c6d8503682172628
Taranaki <55310629+taranakineko@users.noreply.github.com>
2024-01-20 13:39:38 +0800

    Update main.py
    唉，强调一下不需要编辑 README.md 吧（被打

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ markdown = """# review-2023
 
 保持传统，本项目将长期维护（直到2025年初）。
 
-**想要添加您的年终总结？请发 Issue 或编辑 metadata.md 发PR（不需要填写博客ID）。**
+**想要添加您的年终总结？请发 Issue 或编辑 metadata.md 发 PR**
+
+**（不需要填写博客ID，不需要编辑 README.md）。**
 
 ---
 
